17b4e92fb8c271406a76d1015147fd762538bae1
mmm <mayumeng2002@gmail.com>
2024-05-20 23:08:44 +0800

    Changes 实现点赞和取消点赞功能

--- src/main/java/cn/edu/ujn/shortvideo/mapper/LikesMapper.java
+++ src/main/java/cn/edu/ujn/shortvideo/mapper/LikesMapper.java
@@ -1,7 +1,12 @@
 package cn.edu.ujn.shortvideo.mapper;
 
 import cn.edu.ujn.shortvideo.entities.dox.Likes;
+import cn.edu.ujn.shortvideo.entities.dox.Users;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Optional;
 
 /**
 * @author dell
@@ -10,6 +15,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 * @Entity generator.domain.Likes
 */
 public interface LikesMapper extends BaseMapper<Likes> {
+
+    Optional<Likes> findByVideoIdAndUserId(@Param("videoId") Integer videoId, @Param("userId") Integer userId);
+    Likes save(Likes like);
+
+    boolean existsById(Integer likeId);
 
 }
 
