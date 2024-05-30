793b3c11d6308f9bb02a29149e85d29af2181ef9
fayefff <1966582748@qq.com>
2024-05-30 21:43:02 +0800

    添加@Service

--- src/main/java/cn/edu/ujn/shortvideo/service/impl/FollowsServiceImpl.java
+++ src/main/java/cn/edu/ujn/shortvideo/service/impl/FollowsServiceImpl.java
@@ -3,8 +3,9 @@ package cn.edu.ujn.shortvideo.service.impl;
 import cn.edu.ujn.shortvideo.mapper.FollowsMapper;
 import cn.edu.ujn.shortvideo.service.FollowsService;
 import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+@Service
 public class FollowsServiceImpl implements FollowsService {
 
     @Resource
