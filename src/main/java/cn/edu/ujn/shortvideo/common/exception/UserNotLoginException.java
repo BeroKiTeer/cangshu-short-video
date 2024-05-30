e64791fdeda10aca903c172bb3d22e012311f3e8
BeroKiTeer <1377433202@qq.com>
2024-05-30 21:16:29 +0800

    debug common class

--- src/main/java/cn/edu/ujn/shortvideo/common/exception/UserNotLoginException.java
+++ src/main/java/cn/edu/ujn/shortvideo/common/exception/UserNotLoginException.java
@@ -2,9 +2,6 @@ package cn.edu.ujn.shortvideo.common.exception;
 
 public class UserNotLoginException extends BaseException {
 
-    public UserNotLoginException() {
-    }
-
     public UserNotLoginException(String msg) {
         super(msg);
     }
