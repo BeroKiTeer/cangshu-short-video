40a78be8e566e5225143012987c2dbe951387cfe
mmm <mayumeng2002@gmail.com>
2024-06-09 23:33:30 +0800

    Changes FollowsServiceImpl添加注释；点赞功能测试不通过，修正了一下LikesMapper

--- src/main/java/cn/edu/ujn/shortvideo/service/impl/FollowsServiceImpl.java
+++ src/main/java/cn/edu/ujn/shortvideo/service/impl/FollowsServiceImpl.java
@@ -11,15 +11,30 @@ public class FollowsServiceImpl implements FollowsService {
     @Resource
     private FollowsMapper followsMapper;
 
+    /**
+     * 用户关注操作。
+     * 该方法实现了用户关注另一个用户的功能。通过调用followsMapper的insertFollow方法，
+     * 在数据库中插入一条关注记录，从而建立关注关系。
+     *
+     * @param followerId 关注者的用户ID。表示哪个用户进行了关注操作。
+     * @param followedId 被关注者的用户ID。表示哪个用户被关注。
+     */
     @Override
     @Transactional
     public void follow(int followerId, int followedId) {
         followsMapper.insertFollow(followerId, followedId);
     }
 
+
+    /**
+     * 取消关注操作。
+     *
+     * @param followId 被关注者的ID，通过该ID在数据库中查找并删除关注关系。
+     */
     @Override
     @Transactional
     public void unfollow(int followId) {
         followsMapper.deleteFollow(followId);
     }
+
 }
